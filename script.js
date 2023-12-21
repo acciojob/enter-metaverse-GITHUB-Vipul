3774345e2e987a6bbdd255967a34e8eccf72c2aa
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-21 13:18:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function changeTag()
+	{
+		let tag=document.getElementById("status");
+		tag.remove();
+		let ele=document.createElement("h1");
+		ele.innerText="Enter Metaverse";
+		enterBtn.before(ele);
+		
+	}
